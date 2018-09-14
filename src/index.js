8c37e891eceb708692e55ee6dc66d0aa546fadcf
Juan Pablo Ortiz <janpoloy@gmail.com>
2018-09-14 10:43:54 -0500

    eslint and more dev depencies setup

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import * as builder from 'botbuilder';
 // Setting up the server
 const server = restify.createServer();
 server.listen(3978, () => {
- console.log("Server listening to 3978");
+  console.log('Server listening to 3978');
 });
 // Create chat bot
 const connector = new builder.ChatConnector({
- appId: "",
- appPassword: "",
+  appId: '',
+  appPassword: '',
 });
 const bot = new builder.UniversalBot(connector)
-server.post("/api/messages", connector.listen());
-bot.dialog("/", (session) => {
- // Edit more dialog here ❺
- session.send("You said " + session.message.text);
-}) 
+server.post('/api/messages', connector.listen());
+bot.dialog('/', (session) => {
+  // Edit more dialog here ❺
+  session.send(`You said ${session.message.text}`);
+});
