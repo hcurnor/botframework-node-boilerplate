63a5a3e7f590abe20fd7f2fc4ae3792304c253c2
Juan Pablo Ortiz <janpoloy@gmail.com>
2018-09-14 09:30:12 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import restify from 'restify';
+import * as builder from 'botbuilder';
+// Setting up the server
+const server = restify.createServer();
+server.listen(3978, () => {
+ console.log("Server listening to 3978");
+});
+// Create chat bot
+const connector = new builder.ChatConnector({
+ appId: "",
+ appPassword: "",
+});
+const bot = new builder.UniversalBot(connector)
+server.post("/api/messages", connector.listen());
+bot.dialog("/", (session) => {
+ // Edit more dialog here ❺
+ session.send("You said " + session.message.text);
+}) 
