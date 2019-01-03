4fb48edb69e89466ac74ff98f83e542a2011ff65
Juan Pablo <juanpablo@Juans-MacBook-Air.local>
2019-01-03 14:48:45 -0600

    LUIS setup & working

---
+++ src/constants/hoistedLuisIntents.js
@@ -0,0 +1,19 @@
+// Custom intents of the Arly-Core-Intents from LUIS
+export default [
+  'AttentionSchedule',
+  'BotInfo',
+  'CreateIssue',
+  'EventInformation',
+  'Goodbye',
+  'HowTo',
+  'ManageIssue',
+  'None',
+  'PaymentInfo',
+  'ProductAvailability',
+  'ProductInformation',
+  'PromotionsInfo',
+  'RequestInfo',
+  'TalkToHuman',
+  'leaveFeedback',
+  'trackOrder',
+];
--- src/index.js
+++ src/index.js
@@ -11,16 +11,22 @@ require('dotenv').config();
 const inMemoryStorage = new builder.MemoryBotStorage();
 // Create chat bot
 const connector = new builder.ChatConnector({
-  appId: '',
-  appPassword: '',
+  appId: process.env.MICROSOFT_APP_ID || '',
+  appPassword: process.env.MICROSOFT_APP_PASSWORD || '',
 });
 
-const bot = new builder.UniversalBot(connector).set('storage', inMemoryStorage); // Register in memory storage
+const bot = new builder.UniversalBot(connector);
+
+bot.set('storage', inMemoryStorage); // Register in memory storage
+
+bot.set('localizerSettings', { // Configure bots default locale and locale folder path.
+  defaultLocale: 'es',
+});
 
 // Setting up the server
 const server = restify.createServer();
-server.listen(3978, () => {
-  console.log('Server listening to 3978');
+server.listen(process.env.PORT || 3978, () => {
+  console.log(`Server listening to 3978 ${process.env.PORT || 3978}`);
 });
 
 server.post('/api/messages', connector.listen());
@@ -30,19 +36,20 @@ server.post('/api/messages', connector.listen());
 // =========================================================
 
 // Your-LUIS-App-ID
-// const LuisAppID = process.env.LUIS_APP_ID;
+const LuisAppID = process.env.LUIS_APP_ID;
 // Your-LUIS-Key
-// const LuisKey = process.env.LUIS_APP_KEY;
-// const LuisModel = `https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/${LuisAppID}?subscription-key=${LuisKey}`;
-// const recognizer = new builder.LuisRecognizer(LuisModel);
+const LuisKey = process.env.LUIS_APP_KEY;
+const LuisModel = `https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/${LuisAppID}?subscription-key=${LuisKey}`;
+const recognizer = new builder.LuisRecognizer(LuisModel);
 
-// bot.recognizer(recognizer);
+bot.recognizer(recognizer);
 
+// =========================================================
 // Dialogs
-bot.dialog('/', new builder.SimpleDialog((session, result) => {
-  // Edit more dialog here ❺
-  // session.send(`You said ${session.message.text}`);
+// =========================================================
 
+// Default
+bot.dialog('/', new builder.SimpleDialog((session, result) => {
   // Redux store setup
   const store = loadStore(session);
   const { attachments, text } = session.message || {}; // What user sends
@@ -50,3 +57,11 @@ bot.dialog('/', new builder.SimpleDialog((session, result) => {
     store.dispatch(DialogActions.receiveMessage(text, attachments, result));
   }
 }));
+
+bot.dialog('GreetingDialog',
+  (session) => {
+    session.send('You reached the Greeting intent. You said \'%s\'.', session.message.text);
+    session.endDialog();
+  }).triggerAction({
+  matches: 'Greetings',
+});
