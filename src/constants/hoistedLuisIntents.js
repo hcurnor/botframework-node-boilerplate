e0f4eb1cdfc18b31133f0f215041170ce6ddb740
Juan Pablo <juanpablo@Juans-MacBook-Air.local>
2019-01-21 15:53:39 -0600

    more luis intents

--- src/constants/hoistedLuisIntents.js
+++ src/constants/hoistedLuisIntents.js
@@ -12,11 +12,6 @@ export default [
     matches: 'Goodbye',
   },
   {
-    dialog: 'AttentionScheduleDialog',
-    locale: 'bye_messages_generic',
-    matches: 'AttentionSchedule',
-  },
-  {
     dialog: 'ThanksDialog',
     locale: 'thanks_message',
     matches: 'Thanks',
@@ -30,5 +25,55 @@ export default [
     dialog: 'PaymentInfoDialog',
     locale: botConfig.paymentText,
     matches: 'PaymentInfo',
+  },
+  {
+    dialog: 'AttentionScheduleDialog',
+    locale: 'soon_message',
+    matches: 'AttentionSchedule',
+  },
+  {
+    dialog: 'BotInfoDialog',
+    locale: 'soon_message',
+    matches: 'BotInfo',
+  },
+  {
+    dialog: 'CreateIssueDialog',
+    locale: 'soon_message',
+    matches: 'CreateIssue',
+  },
+  {
+    dialog: 'EventInformationDialog',
+    locale: 'soon_message',
+    matches: 'EventInformation',
+  },
+  {
+    dialog: 'HowToDialog',
+    locale: 'soon_message',
+    matches: 'HowTo',
+  },
+  {
+    dialog: 'ManageIssueDialog',
+    locale: 'soon_message',
+    matches: 'ManageIssue',
+  },
+  {
+    dialog: 'PriceInfoDialog',
+    locale: 'soon_message',
+    matches: 'PriceInfo',
+  },
+  {
+    dialog: 'ProductAvailabilityDialog',
+    locale: 'soon_message',
+    matches: 'ProductAvailability',
+  },
+  {
+    dialog: 'PromotionsInfoDialog',
+    locale: 'soon_message',
+    matches: 'PromotionsInfo',
+  },
+  {
+    dialog: 'leaveFeedbackDialog',
+    locale: 'soon_message',
+    matches: 'leaveFeedback',
   },
 ];
